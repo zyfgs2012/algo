b5859c28f19cd63d97676c29182d2093fc7ffdb9
zyfgs2012 <zyfgs2012@163.com>
2014-11-07 20:39:20 +0800

    invasive slink and dlink

---
+++ ads/link.h
@@ -0,0 +1,209 @@
+#ifndef LINK_H
+#define LINK_H
+
+/**invasive slink and dlink :has a dummy header*/
+typedef struct __SLINK{
+	__SLINK *next;
+}SLINK;
+
+#define SLINK_INIT(head)	((head)->next = NULL)
+
+#define SLINK_IS_EMPTY(head)	((head)->next == NULL)
+
+#define SLINK_TOP(head)	(head)->next
+
+#define SLINK_POP(head)	(head)->next;							\
+	                    if(NULL != (head)->next) {				\
+                            (head)->next = (head)->next->next;	\
+						}
+
+#define SLINK_PUSH(head,node)	(node)->next = (head)->next;	\
+								(head)->next = (node)
+
+
+typedef struct __DLINK{
+	__DLINK *prev;
+	__DLINK *next;
+}DLINK;
+
+#define DLINK_INIT(head)	(head)->prev = (head)->next = (head)
+
+#define DLINK_IS_EPMTY(head)	((head)->next == head && (head)->prev == head)
+
+#define DLINK_INSERT_NEXT(head,node)	(node)->next = (head)->next;\
+										(node)->prev =  (head);\
+										(head)->next->prev = (node);\
+										(head)->next = (node)
+
+#define DLINK_INSERT_PREV(head,node)	(node)->prev = (head)->prev;\
+										(node)->next =  (head);\
+										(head)->prev->next = (node);\
+										(head)->prev = (node)
+
+#define DLINK_REMOVE(node)		(node)->prev->next = (node)->next;\
+								(node)->next->prev = (node)->prev
+
+#define DLINK_REPLACE(_old,_new)	(_old)->prev->next = (_new);\
+									(_old->next->prev) = (_new);\
+									(_new)->prev = (_old)->prev;\
+									(_new)->next = (_old)->next
+
+//get address of structure type
+#define RECORD_OF(address,type,field) \
+	(type*)((char*)address - (char*)&((type*)0)->field)
+
+
+
+/*
+ * template SLINK
+ * T must contain SLINK link
+*/
+template <typename T>
+class SList {
+	protected:
+		int m_count;
+		SLINK m_head;
+	public:
+		SList() {
+			SLINK_INIT(&m_head);
+			m_count = 0;
+		}
+		~SList() {}
+		void push(T *e) {
+			SLINK_PUSH(&m_head,&e->link);
+			m_count++;
+		}
+
+		T* pop() {
+			SLINK *p = SLINK_POP(&m_head);
+			if(NULL != p) {
+				m_count--;
+				SLINK_INIT(p);
+				return RECORD_OF(p,T,link);
+			}
+			return NULL;
+		}
+
+		T* top() {
+			SLINK *p = m_head.next;
+			if(NULL != p) {
+				return RECORD_OF(p,T,link);
+			}
+			return NULL;
+		}
+		
+		bool contain(T* e) {
+			SLINK *p;
+			for(p = m_head.next;p;p = p->next) {
+				if(&e->link == p) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		bool remove(T* e) {
+			SLINK *p = &m_head;
+			while(NULL != p->next) {
+				if(p->next == &e->link) {
+					p->next = p->next->next;
+					SLINK_INIT(&e->link);
+					m_count--;
+					return true;
+				}
+				p = p->next;
+			}
+			return false;
+		}
+		
+		int size() {
+			return m_count;
+		}
+};
+
+template <typename T>
+class DList {
+	protected:
+		int m_count;
+		DLINK m_head;
+	public:
+		DList() {
+			DLINK_INIT(&m_head);
+			m_count = 0;
+		};
+		~DList() {}
+		void push_front(T* e) {
+			DLINK_INSERT_NEXT(&m_head,&e->link);
+			m_count++;
+		}
+
+
+		void push_back(T* e) {
+			DLINK_INSERT_PREV(&m_head,&e->link);
+			m_count++;
+		}
+
+		T* pop_front() {
+			DLINK *p = m_head.next;
+			if(p != &m_head) {
+				DLINK_REMOVE(p);
+				DLINK_INIT(p);
+				m_count--;
+				return RECORD_OF(p,T,link);
+			}
+			return NULL;
+		}
+
+		T* pop_back() {
+			DLINK *p = m_head.prev;
+			if(p != &m_head) {
+				DLINK_REMOVE(p);
+				DLINK_INIT(p);
+				m_count--;
+				return RECORD_OF(p,T,link);
+			}
+			return NULL;
+		}
+
+		T* top() {
+			DLINK *p = m_head.next;
+			if(p != &m_head) {
+				return RECORD_OF(p,T,link);
+			}
+			return NULL;
+		}
+		
+		T* bottom() {
+			DLINK *p = m_head.prev;
+			if(p != &m_head) {
+				return RECORD_OF(p,T,link);
+			}
+			return NULL;
+		}
+
+		bool contain(T* e) {
+			for(DLINK* p = m_head.next;p != &m_head;p = p->next) {
+				if(&e->link == p) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		bool remove(T* e) {
+			if(!contain(e)) {
+				printf("[%s:%d]:%s\n",__FILE__,__LINE__,"element non-existing");
+				return false;
+			}
+			DLINK_REMOVE(&e->link);
+			DLINK_INIT(&e->link);
+			m_count--;
+			return true;
+		}
+		
+		int size() {
+			return m_count;
+		}
+};
+
+#endif
---
+++ test/link_test.cpp
@@ -0,0 +1,99 @@
+#include <cstdio>
+#include "../ads/link.h"
+
+struct STest {
+	int val;
+	SLINK link;
+};
+
+struct DTest {
+	int val;
+	DLINK link;
+};
+
+int main() {
+#if 0
+	SList<STest> s;
+	STest st[7];
+	st[0].val = 0;
+	st[1].val = 1;
+	st[2].val = 2;
+	st[3].val = 3;
+	st[4].val = 4;
+	st[5].val = 5;
+	st[6].val = 6;
+	s.push(&st[0]);
+	s.push(&st[1]);
+	s.push(&st[2]);
+	s.push(&st[3]);
+	s.push(&st[4]);
+	s.push(&st[5]);
+	s.push(&st[6]);
+	printf("\n");
+	int n = s.size();
+	for(int i=0;i<=3;i++) {
+		printf("top:%d,contains:%d:%d\n",s.top()->val,i,s.contain(&st[i]));
+		s.pop();
+	}
+	s.remove(&st[0]);
+	n = s.size();
+	printf("contain 0:%d,size:%d\n",s.contain(&st[0]),n);
+	printf("top:%d\n",s.top()->val);
+	s.remove(&st[2]);
+	n = s.size();
+	printf("contain 2:%d,size:%d\n",s.contain(&st[2]),n);
+	printf("top:%d\n",s.top()->val);
+
+#elif 1
+
+	DList<DTest> d;
+	DTest dt[6];
+	dt[1].val = 1;
+	dt[2].val = 2;
+	dt[3].val = 3;
+	dt[4].val = 4;
+	dt[5].val = 5;
+	
+	d.push_front(&dt[3]);
+	d.push_back(&dt[4]);
+	d.push_front(&dt[2]);
+	d.push_back(&dt[5]);
+	d.push_front(&dt[1]);
+	int n = d.size();
+	for(int i=0;i<n;i++) {
+		printf("bottom:%d,contains:%d:%d\n",d.bottom()->val,i,d.contain(&dt[i]));
+		d.pop_back();
+	}
+	printf("size:%d\n",d.size());
+
+	d.push_back(&dt[3]);
+	d.push_front(&dt[2]);
+	d.push_back(&dt[4]);
+	d.push_front(&dt[1]);
+	d.push_back(&dt[5]);
+	n = d.size();
+	for(int i=0;i<n;i++) {
+		printf("top:%d,contains:%d:%d\n",d.top()->val,5-i,d.contain(&dt[5-i]));
+		d.pop_front();
+	}
+	printf("size:%d\n",d.size());
+
+	d.push_front(&dt[5]);
+	d.push_front(&dt[4]);
+	d.push_front(&dt[3]);
+	d.push_front(&dt[2]);
+	d.push_front(&dt[1]);
+	d.remove(&dt[0]);
+	d.remove(&dt[1]);
+	d.remove(&dt[3]);
+	d.remove(&dt[5]);
+	n = d.size();
+	for(int i=0;i<n;i++) {
+		printf("bottom:%d\n",d.bottom()->val);
+		d.pop_back();
+	}
+	printf("size:%d\n",d.size());
+
+#endif
+	return 0;
+}
